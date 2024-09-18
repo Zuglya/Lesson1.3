c2ce4fd4891f116d79ab16e9467602ab5fd791d6
Zuglya <irussel86@gmail.com>
2024-09-19 00:28:28 +0400

    Добавлен Lesson1.3

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("******************************\n");
+        System.out.print("*******     Визитка    *******\n");
+        System.out.print("******* name: \"Russel\" *******\n");
+        System.out.print("******* Skype: Huyayp  *******\n");
+        System.out.print("******************************");
+    }
+}
